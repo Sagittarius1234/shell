d6f701aa36c433204b2f8f6829ba0210b264c0f4
caroldesenne <desennecarol@gmail.com>
2016-12-07 17:25:31 +0100

    Revert "test 16 doesnt work! fuck"
    This reverts commit 544af9ace7d5e7a28acc7a50f0f6d0335d05fac5.

--- tsh.c
+++ tsh.c
@@ -362,7 +362,8 @@ void do_bgfg(char **argv)
 void waitfg(pid_t pid)
 {
     int status;
-    waitpid(pid,&status,WUNTRACED);
+    if (waitpid(pid,&status,WUNTRACED)<0)
+        unix_error("waitfg: waitpid error");
     if (!WIFSTOPPED(status))
         deletejob(jobs,pid);
     return;
@@ -383,24 +384,8 @@ void sigchld_handler(int sig)
 {
     int status;
     pid_t pid;
-    struct job_t *job;
-    write(1,"chld\n",5);
-    while ((pid = waitpid(-1,&status,WUNTRACED|WCONTINUED)) > 0) {
-        job=getjobpid(jobs,pid);
-        if (job == NULL) return;
-        if (WIFSIGNALED(status)) {
-            printf("Job [%d] (%d) terminated by signal %d\n",job->jid,pid,WTERMSIG(status));
-            deletejob(jobs,pid);
-        } else if (WIFSTOPPED(status)) {
-            printf("Job [%d] (%d) stopped by signal %d\n",job->jid,pid,WSTOPSIG(status));
-            job->state = ST;
-        } else if (WIFCONTINUED(status)) {
-            printf("Job [%d] (%d) continued by signal %d\n",job->jid,pid,SIGCONT);
-            job->state = BG;
-        } else if (WIFEXITED(status)) {
-            deletejob(jobs,pid);
-        }
-    }
+    while ((pid = waitpid(-1,&status,WNOHANG)) > 0)
+        deletejob(jobs,pid);
     return;
 }
 
